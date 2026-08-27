1296ddab525fea22076b72c6a01d6d9a95d3c1d1
habibhanafi <muhammadhabib348@gmail.com>
2026-08-27 11:00:02 +0700

    Update script.js

--- SKBT Digital/script.js
+++ SKBT Digital/script.js
@@ -171,7 +171,7 @@ if (keperluan) {
 
       death =
         this.value ===
-        "Pemberhentian dengan hormat sebagai Pegawai Negeri Sipil (PNS) karena meninggal dunia.";
+        "Pemberhentian dengan hormat sebagai Pegawai Negeri Sipil (PNS) karena meninggal dunia";
 
 
       const heirSection =
